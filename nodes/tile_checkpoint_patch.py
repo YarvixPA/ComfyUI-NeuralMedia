b1734d93dddcbbb8fbaa4911f2fa6059469f4dbe
YarvixPA <152553832+YarvixPA@users.noreply.github.com>
2025-05-09 05:59:08 -0500

    Update tile_checkpoint_patch.py

--- nodes/tile_checkpoint_patch.py
+++ nodes/tile_checkpoint_patch.py
@@ -19,7 +19,7 @@ class TileCheckpointPatchNode:
 
     RETURN_TYPES = ("MODEL", "VAE")  # Removed CLIP from the outputs
     FUNCTION = "tile_checkpoint_patch"
-    CATEGORY = "ComfyUI-NeuralMedia/model/unet/patches"
+    CATEGORY = "ComfyUI-NeuralMedia/model/checkpoint/patches"
 
     def tile_checkpoint_patch(self, model, vae, tiling, copy_option):
         # Duplicate or modify in place
